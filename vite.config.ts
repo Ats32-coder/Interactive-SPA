18da397a912224d6942df4b141c1e14580f72d75
Ats32-coder <aadukubja@gmail.com>
2026-04-22 02:44:09 +0300

    live

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repositoryName = 'Interactive-SPA';
+
 export default defineConfig({
+  base: `/${repositoryName}/`,
   plugins: [react()],
 });
